922f91eaaad133985cb0219127a0c009079c75f1
phamson197 <phamson11999977@gmail.com>
2018-03-09 13:38:20 +0700

    Add files via upload

--- create quan ly kho.sql
+++ create quan ly kho.sql
@@ -1,15 +1,29 @@
+USE master;
+GO
+ALTER DATABASE [Quan ly kho] SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+GO
+drop database if exists [Quan ly kho]
+create database [Quan ly kho]
+go
 use [Quan ly kho]
 go
+
+drop table if exists NhaCungCap
+create table NhaCungCap (
+NhaCC_ID varchar(20) not null primary key,
+NhaCC_ten nvarchar(40)
+)
 
 drop table if exists HangHoa
 create table HangHoa (
 HangHoa_ID varchar(20) not null primary key,
 HangHoa_ten nvarchar(40),
+NhaCC_ID varchar(20) not null foreign key references NhaCungCap(NhaCC_ID)
 )
 
 drop table if exists PhieuNhap
 create table PhieuNhap (
-PhieuNhap_ID varchar(20) not null primary key,
+PN_ID varchar(20) not null primary key,
 NgayNhap date,
 HangHoa_ID varchar(20) not null foreign key references HangHoa(HangHoa_ID),
 SoLuong int
@@ -17,14 +31,8 @@ SoLuong int
 
 drop table if exists PhieuXuat
 create table PhieuXuat (
-PhieuXuat_ID varchar(20) not null primary key,
+PX_ID varchar(20) not null primary key,
 NgayXuat date,
 HangHoa_ID varchar(20) not null foreign key references HangHoa(HangHoa_ID),
 SoLuong int
-)
-
-drop table if exists NhaCungCap
-create table NhaCungCap (
-NhaCC_ID varchar(20) not null primary key,
-NhaCC_ten nvarchar(40)
 )
