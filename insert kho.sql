e662db517df375fef707abba5290b32b0e93a726
PhamSon <phamson11999977@gmail.com>
2018-03-20 10:45:04 +0700

    Add files via upload

--- insert kho.sql
+++ insert kho.sql
@@ -1,17 +1,6 @@
 use [Quan ly kho]
 go
 
-insert dbo.HangHoa (
-HangHoa_ID,HangHoa_ten,NhaCC_ID) values ('00001','GPU','0001')
-insert dbo.HangHoa (
-HangHoa_ID,HangHoa_ten,NhaCC_ID) values ('00002','Monitor','0002')
-insert dbo.HangHoa (
-HangHoa_ID,HangHoa_ten,NhaCC_ID) values ('00003','Mainboard','0003')
-insert dbo.HangHoa (
-HangHoa_ID,HangHoa_ten,NhaCC_ID) values ('00004','RAM','0004')
-insert dbo.HangHoa (
-HangHoa_ID,HangHoa_ten,NhaCC_ID) values ('00005','CPU','0005')
-
 insert dbo.NhaCungCap (
 NhaCC_ID, NhaCC_ten) values ('0001','Cty Duc Duong')
 insert dbo.NhaCungCap (
@@ -23,25 +12,38 @@ NhaCC_ID, NhaCC_ten) values ('0004','Cty Co phan thuong mai')
 insert dbo.NhaCungCap (
 NhaCC_ID, NhaCC_ten) values ('0005','Cty Sunshine')
 
+insert dbo.HangHoa (
+HangHoa_ID,HangHoa_ten,NhaCC_ID) values ('00001','GPU','0001')
+insert dbo.HangHoa (
+HangHoa_ID,HangHoa_ten,NhaCC_ID) values ('00002','Monitor','0002')
+insert dbo.HangHoa (
+HangHoa_ID,HangHoa_ten,NhaCC_ID) values ('00003','Mainboard','0003')
+insert dbo.HangHoa (
+HangHoa_ID,HangHoa_ten,NhaCC_ID) values ('00004','RAM','0004')
+insert dbo.HangHoa (
+HangHoa_ID,HangHoa_ten,NhaCC_ID) values ('00005','CPU','0005')
+
 insert dbo.PhieuNhap (
-PN_ID, HangHoa_ID, NgayNhap, SoLuong) values ('001','00001',2017-3-9,6)
+PN_ID, HangHoa_ID, NgayNhap, SoLuong) values ('001','00001','2017-3-9',6)
 insert dbo.PhieuNhap (
-PN_ID, HangHoa_ID, NgayNhap, SoLuong) values ('001','00001',2017-3-9,6)
+PN_ID, HangHoa_ID, NgayNhap, SoLuong) values ('002','00002','2017-3-9',6)
 insert dbo.PhieuNhap (
-PN_ID, HangHoa_ID, NgayNhap, SoLuong) values ('001','00001',2017-3-9,6)
+PN_ID, HangHoa_ID, NgayNhap, SoLuong) values ('003','00003','2017-3-9',6)
 insert dbo.PhieuNhap (
-PN_ID, HangHoa_ID, NgayNhap, SoLuong) values ('001','00001',2017-3-9,6)
+PN_ID, HangHoa_ID, NgayNhap, SoLuong) values ('004','00004','2017-3-9',6)
 insert dbo.PhieuNhap (
-PN_ID, HangHoa_ID, NgayNhap, SoLuong) values ('001','00001',2017-3-9,6)
+PN_ID, HangHoa_ID, NgayNhap, SoLuong) values ('005','00005','2017-3-9',6)
 
 insert dbo.PhieuXuat (
-PX_ID, HangHoa_ID, NgayXuat, SoLuong) values ('001','00001',2017-3-9,6)
+PX_ID, HangHoa_ID, NgayXuat, SoLuong) values ('001','00001','2017-3-9',6)
 insert dbo.PhieuXuat (
-PX_ID, HangHoa_ID, NgayXuat, SoLuong) values ('002','00002',2017-3-9,6)
+PX_ID, HangHoa_ID, NgayXuat, SoLuong) values ('002','00002','2017-3-9',6)
 insert dbo.PhieuXuat (
-PX_ID, HangHoa_ID, NgayXuat, SoLuong) values ('003','00003',2017-3-9,6)
+PX_ID, HangHoa_ID, NgayXuat, SoLuong) values ('003','00003','2017-3-9',6)
 insert dbo.PhieuXuat (
-PX_ID, HangHoa_ID, NgayXuat, SoLuong) values ('004','00004',2017-3-9,6)
+PX_ID, HangHoa_ID, NgayXuat, SoLuong) values ('004','00004','2017-3-9',6)
 insert dbo.PhieuXuat (
-PX_ID, HangHoa_ID, NgayXuat, SoLuong) values ('005','00005',2017-3-9,6)
+PX_ID, HangHoa_ID, NgayXuat, SoLuong) values ('005','00005','2017-3-9',6)
 
+
+delete from NhaCungCap
